19303e14e39e8310c9790ee51f65711aae0dc058
leo-d-a <damerval.leo@proton.me>
2024-03-20 16:14:46 +0100

    feat: display a linechart with the data

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,11 +4,36 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import data from './data.json';
 
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+
 function App() {
   const [items, setItems] = useState(data.results.items);
 
+  //convert the "tradedatetimegmt" to a date object
+  /* items.forEach((item) => {
+    item.tradedatetimegmt = new Date(item.tradedatetimegmt);
+  }); */
+
   return (
     <>
+      <div>
+        //create a line chart with the data from the items array with the"close"
+        as the y-axis and converted "tradedatetimegmt" as the x-axis
+        <LineChart
+          width={800}
+          height={400}
+          data={items}
+        >
+          <Line
+            type="monotone"
+            dataKey="close"
+            stroke="#8884d8"
+          />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis dataKey="tradedatetimegmt" />
+          <YAxis />
+        </LineChart>
+      </div>
       <div>
         //for each element in the items array, create a line in a table with the
         item's "close", "offexchtradevolumeeex" ,"onexchtradevolumeeex",
